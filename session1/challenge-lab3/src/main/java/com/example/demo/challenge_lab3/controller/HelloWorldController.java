109b82d499d178500579fad642097e54d959bc7e
Gidss-28 <gideon-a.salangsang@gcash.com>
2025-07-16 15:39:11 +0800

    Final commit: challenge 3

--- session1/challenge-lab3/src/main/java/com/example/demo/challenge_lab3/controller/HelloWorldController.java
+++ session1/challenge-lab3/src/main/java/com/example/demo/challenge_lab3/controller/HelloWorldController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @RestController // Marks this class as a REST controller
 public class HelloWorldController {
@@ -35,4 +40,17 @@ public class HelloWorldController {
         int sum = number1 + number2; return "The sum of " + number1 + " and " + number2 + " is " + sum;
     }
 
+    @GetMapping("/info")
+    public Map<String, String> getAppInfo() {
+        Map<String, String> info = new HashMap<>();
+        info.put("appName", "MyFirstSpringBootAPI");
+        info.put("version", "1.0.0");
+        info.put("status", "Running");
+        return info;
+    }
+
+    @GetMapping("/features")
+    public List<String> getFeatures() {
+        return Arrays.asList("REST API", "Spring Boot", "Easy Setup", "Fast Development");
+    }
 }
